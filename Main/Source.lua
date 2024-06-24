c49b007d1566c98a173a8d5410a140183120f2a9
Weeblych <160259130+liquidcheats@users.noreply.github.com>
2024-06-24 21:15:46 +0800

    Update Source.lua
    fixed shit

--- Main/Source.lua
+++ Main/Source.lua
@@ -1269,8 +1269,6 @@ function MobileToggle(keybind, icon)
     menu.BorderSizePixel = 0
     menu.Position = UDim2.new(0.134796232, 0, 0.531400979, 0)
     menu.Size = UDim2.new(0, 44, 0, 45)
-    
-    Dragify(menu, MobTogg)
 
     UICorner.CornerRadius = UDim.new(0, 12)
     UICorner.Parent = menu
@@ -1295,11 +1293,61 @@ function MobileToggle(keybind, icon)
     IconButton.Text = ""
     IconButton.TextColor3 = Color3.fromRGB(0, 0, 0)
     IconButton.TextSize = 14.000
+
     IconButton.MouseButton1Click:Connect(function()
         Library:Toggle()
     end)
 
-    task.wait()
+    local UserInputService = game:GetService("UserInputService")
+
+    local dragging
+    local dragInput
+    local dragStart
+    local startPos
+
+    function Lerp(a, b, m)
+        return a + (b - a) * m
+    end
+
+    local lastMousePos
+    local lastGoalPos
+    local DRAG_SPEED = 9.2
+    function Update(dt)
+        if not startPos then return end
+        if not dragging and lastGoalPos then
+            menu.Position = UDim2.new(startPos.X.Scale, Lerp(menu.Position.X.Offset, lastGoalPos.X.Offset, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(menu.Position.Y.Offset, lastGoalPos.Y.Offset, dt * DRAG_SPEED))
+            return
+        end
+
+        local delta = lastMousePos - UserInputService:GetMouseLocation()
+        local xGoal = startPos.X.Offset - delta.X
+        local yGoal = startPos.Y.Offset - delta.Y
+        lastGoalPos = UDim2.new(startPos.X.Scale, xGoal, startPos.Y.Scale, yGoal)
+        menu.Position = UDim2.new(startPos.X.Scale, Lerp(menu.Position.X.Offset, xGoal, dt * DRAG_SPEED), startPos.Y.Scale, Lerp(menu.Position.Y.Offset, yGoal, dt * DRAG_SPEED))
+    end
+
+    menu.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+            dragging = true
+            dragStart = input.Position
+            startPos = menu.Position
+            lastMousePos = UserInputService:GetMouseLocation()
+
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+
+    menu.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+            dragInput = input
+        end
+    end)
+
+    game:GetService("RunService").Heartbeat:Connect(Update)
 end
 
 return Library
